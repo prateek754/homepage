4928c904e958bb33788810a68e117b9bdecd9d52
prateek754 <prateek6289@gmail.com>
2022-08-29 20:13:33 -0600

    'netifly'

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://prateek754.github.io/homepage',
+  site: 'https://prateeksahu.netlify.app',
   // Your public domain, e.g.: https://prateeksahu.com/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
