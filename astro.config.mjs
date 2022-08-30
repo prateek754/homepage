06288c44956b901d2308e6109e7627de3508b4c0
prateek754 <prateek6289@gmail.com>
2022-08-29 19:55:48 -0600

    'netifly'

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,6 @@ import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
 
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
-
-export default defineConfig({
-  output: 'server',
-	adapter: netlify(),
-});
 
 // https://astro.build/config
 export default defineConfig({
