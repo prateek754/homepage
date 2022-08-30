852e2580f48c13b6d40af83f99f9d6e17348ec21
prateek754 <prateek6289@gmail.com>
2022-08-29 20:08:59 -0600

    'netifly'

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
 	title: `Prateek Sahu`,
 	description: ``,
-	url: `https://prateeksahu.com`, // No trailing slash!
+	url: `https://prateek754.github.io/homepage`, // No trailing slash!
 	name: `Prateek Sahu`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
 };
