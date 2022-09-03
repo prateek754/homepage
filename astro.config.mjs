aeb76ace85488874ff2a515660165da47a2b0c09
Prateek Sahu <prateek6289@gmail.com>
2022-09-03 11:31:34 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://prateeksahu.com/',
+  site: 'https://prateeksahu.com',
   // Your public domain, e.g.: https://prateeksahu.com/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
